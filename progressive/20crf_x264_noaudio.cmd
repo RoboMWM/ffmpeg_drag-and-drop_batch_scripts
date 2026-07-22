8b1334a786bcc26fd2948716112f8f5db8a24a21
Server <19244210+MLG-SERBUR@users.noreply.github.com>
2026-07-22 16:06:05 -0700

    feat(dumcord): add 20crf x264 encoding script without audio

--- progressive/20crf_x264_noaudio.cmd
+++ progressive/20crf_x264_noaudio.cmd
@@ -3,7 +3,7 @@ cd /d "%~dp0"
 
 REM Progressive encoding
 set "VIDEO_ENCODER=libx264 -crf 20 -preset veryslow"
-set "AUDIO_ENCODER=aac -b:a 192k"
+set "AUDIO_ENCODER=copy -an"
 set "OUTPUT_SUFFIX=20crf"
 set "OUTPUT_EXT=.mp4"
 
