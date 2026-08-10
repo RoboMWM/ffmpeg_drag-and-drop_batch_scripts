a1abdf2d143a05aa5ba30b0255c7c292f1021f69
Server <19244210+MLG-SERBUR@users.noreply.github.com>
2026-08-09 22:55:17 -0700

    fix(crf4): correct video encoder flags for interlaced encoding

--- interlaced/crf4.cmd
+++ interlaced/crf4.cmd
@@ -5,7 +5,7 @@ REM CRF 4 Interlaced (Mezzanine/Archival)
 REM -g 1: All-Intra for maximum fidelity and editability
 REM -pix_fmt yuv422p: 4:2:2 chroma subsampling (high profile)
 REM -flags +ildct+ilme -top 1: Interlaced flags (TFF)
-set "VIDEO_ENCODER=libx264 -preset veryslow -crf 4 -g 1 -x264opts "aq-mode=2:no-fast-pskip=1:no-dct-decimate=1" -pix_fmt yuv422p -flags +ildct+ilme -top 1"
+set "VIDEO_ENCODER=libx264 -preset veryslow -crf 4 -g 1 -x264opts "aq-mode=2:no-fast-pskip=1:no-dct-decimate=1" -pix_fmt yuv422p -vf setfield=tff -flags +ildct+ilme"
 set "AUDIO_ENCODER=libopus -b:a 96k -vbr on"
 set "OUTPUT_SUFFIX=-crf4"
 set "OUTPUT_EXT=.mp4"
